a8d4b347261fa1415a2c6f6165c3fc78159c6257
Boniface <bm715452@gmail.com>
2025-05-20 12:47:05 +0300

    final commit

---
+++ lib/app.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self, name, age, email):
+        self.name = name
+        self.age = age
+        self.email = email
+
+bob = Person("Bob", 29, "bob@flatironschool.com")
+avi = Person("Avi", 28, "avi@flatironschool.com")
+adam = Person("Adam", 28, "adam@flatironschool.com")
